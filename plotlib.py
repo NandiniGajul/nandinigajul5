c6a2197b5234d268317acb1a3b1e9ad190979779
NandiniGajul <88711135+NandiniGajul@users.noreply.github.com>
2022-01-08 20:39:38 +0530

    Update plotlib.py

--- plotlib.py
+++ plotlib.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 #plt.plot([1,2,3,4],[4,8,6,1],'-o') #adding dot and line
 #plt.plot([5,6,7,8],'-go') #addind red and dot line
 #plt.plot([9,10,11,12],'-ro')
-#plt.title("Design by amogh")
+#plt.title("Design by Nandini")
 #fig, ax = plt.subplots()  # Create a figure containing a single axes.
 #ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
 #plt.plot([1,2,3,44,5,])
@@ -16,7 +16,7 @@ y=[100,95,88]
 plt.plot(x,y,label="vedant")
 
 y1=[13,55,89]
-plt.plot(x,y1,label="shruti")
+plt.plot(x,y1,label="Nandini")
 
 y2=[40,45,88]
 plt.plot(x,y2,label="pranav")
